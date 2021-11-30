5649ae2aca2720220d290e998173f30e96fed696
Andrew Martins <andrew_omartins@yahoo.com>
2021-11-30 18:29:53 -0500

    Studying how to solve this using the reducer function

--- LeetCode/valleyCount.js
+++ LeetCode/valleyCount.js
@@ -1,11 +1,11 @@
-// a hiker keeps a meticulous record of his hikes, and for everystep it was noted that 'U' if uphill and 'D' if downhill
-//The hike always starts at sea level and each step up or down represents 1 unit change in altitude 
-// A mountainous sequence always starts up and ends down
-//a valley sequence always starts down and ends up
-// given the sequence, return the number of valleys walked through
+// a hiker keeps a meticulous record of his hikes, and for everystep it was noted that 'U' is uphill and 'D' is downhill.
+//The hike always starts at sea level, and each step up or down represents 1 unit change in altitude. 
+// A mountainous sequence always starts up and ends down.
+//A valley sequence always starts down and ends up.
+// Given the sequence, return the number of valleys walked through.
 //ex: 8
 //    UDDDUDUU
-// OUTPUT: 1
+//    OUTPUT: 1
 
 
 function countingValleys(steps, path) {
@@ -20,4 +20,16 @@ function countingValleys(steps, path) {
     
     return valleyCount;
         
+}
+
+//Reducer Function
+
+function countingValleys(n, s) {
+  let score = 0;
+
+  return [...s].reduce((sum, value, index, array) => {
+      if (value === 'U') { score++; } else { score--;}
+      if (value === 'U' && score === 0) { sum++; }
+      return sum;
+  }, 0);
 }
