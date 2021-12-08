de028eb9fab5473945cdd779e1d09c3ebc2bfe9e
Andrew Martins <andrew_omartins@yahoo.com>
2021-12-07 22:51:28 -0500

    reviewing solutions

--- LeetCode/repeatedString.js
+++ LeetCode/repeatedString.js
@@ -9,3 +9,25 @@ let remainderCount = (s.substring(0, remainder).match(/a/g) || []).length
 return count + remainderCount
 
 }
+
+
+//study this solution
+
+function repeatedString(s, n) {
+    let fracNumber = Math.trunc(n / s.length);
+    let reminder = n % s.length;
+    let counter = 0;
+
+    for (let index = 0; index < s.length; index++) {
+        if (s[index] === 'a') {
+            counter++;
+        }
+    }
+    counter = counter * fracNumber;
+    for (let index = 0; index < reminder; index++) {
+        if (s[index] === 'a') {
+            counter++;
+        }
+    }
+    return counter;
+}
